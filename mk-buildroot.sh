1ac6271b9b97d55841c356b7b6a3d56a3595578c
James Deng <csjamesdeng@lindeni.com>
2018-09-25 19:53:07 +0800

    copy platform and board rootfs overlay to out

--- mk-buildroot.sh
+++ mk-buildroot.sh
@@ -5,7 +5,24 @@
 
 source build/setting.mk
 
-br_out_dir=$OUT_DIR/$_TARGET_PLATFORM/$_TARGET_BOARD/buildroot
+platform_dir=$DEVICE_DIR/$_TARGET_PLATFORM
+board_dir=$platform_dir/boards/$_TARGET_BOARD
+out_dir=$OUT_DIR/$_TARGET_PLATFORM/$_TARGET_BOARD
+br_out_dir=$out_dir/buildroot
+
+[ -d $out_dir ] || mkdir -p $out_dir
+
+if [ -d $platform_dir/rootfs_overlay ] ; then
+	cp -r $platform_dir/rootfs_overlay $out_dir
+fi
+
+if [ -d $board_dir/rootfs_overlay ] ; then
+	cp -r $board_dir/rootfs_overlay $out_dir
+fi
+
+if [ -f $board_dir/module.install ] ; then
+	cp $board_dir/module.install $out_dir
+fi
 
 eval $(sed -n '/^config/p' $DEVICE_DIR/$_TARGET_PLATFORM/boards/$_TARGET_BOARD/buildroot.config)
 [ ! -f $BR_DIR/configs/$config ] && echo "No config exist ($config)" && exit -1
