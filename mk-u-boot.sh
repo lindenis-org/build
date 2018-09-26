c77465588ef81d0eb6ea6ecbffd03fd6a28a5b6b
James Deng <csjamesdeng@lindeni.com>
2018-09-26 16:29:31 +0800

    mk-u-boot.sh: support build various target obj
    including nor, spl (boot0), fes.

--- mk-u-boot.sh
+++ mk-u-boot.sh
@@ -29,7 +29,37 @@ function prepare_toolchain()
 	export CROSS_COMPILE=${toolchain_dir}/bin/arm-linux-gnueabi-
 }
 
+function usage()
+{
+	echo "Usage:"
+	echo "    $ ./build/mk-u-boot.sh"
+	echo "    $ ./build/mk-u-boot.sh nor"
+	echo "    $ ./build/mk-u-boot.sh spl"
+	echo "    $ ./build/mk-u-boot.sh fes"
+}
+
 prepare_toolchain
 
+if [ "x$1" = "xnor" ] ; then
+	make -C $UBOOT_DIR distclean
+	make -C $UBOOT_DIR ${_TARGET_CHIP}_nor_config
+	make -C $UBOOT_DIR -j$PARALLEL_JOBS
+	exit $?
+elif [ "x$1" = "xspl" ] ; then
+	make -C $UBOOT_DIR distclean
+	make -C $UBOOT_DIR ${_TARGET_CHIP}_config
+	make -C $UBOOT_DIR -j$PARALLEL_JOBS spl
+	exit $?
+elif [ "x$1" = "xfes" ] ; then
+	make -C $UBOOT_DIR distclean
+	make -C $UBOOT_DIR ${_TARGET_CHIP}_config
+	make -C $UBOOT_DIR -j$PARALLEL_JOBS fes
+	exit $?
+elif [ -n "$1" ] ; then
+	usage
+	exit -1
+fi
+
+make -C $UBOOT_DIR distclean
 make -C $UBOOT_DIR ${_TARGET_CHIP}_config
 make -C $UBOOT_DIR -j$PARALLEL_JOBS
